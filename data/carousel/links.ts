2a19aa2b0c621effe04c794d0dcf91fb7f9ae4c2
Christian <christian.placido@stu.fondazionejobsacademy.org>
2025-12-20 16:47:13 +0000

    Carosello: aggiunto nuovo link immagine

--- data/carousel/links.ts
+++ data/carousel/links.ts
@@ -9,4 +9,5 @@ export const CAROUSEL_LINKS: string[] = [
   'https://lh3.googleusercontent.com/gps-cs-s/AG0ilSy5_2h6JGS0Xelj1BvMBSNd_7JIoMbX3LeqfdLNOeyJ44BVtctIy_S3rbzz8xTPmGak1ToMhK6_BVtNyUS7O5y7uWGMxdZFPZnThAAjT5Gzu3azdvpphzIcfghNi3Ri9Rs4jCRP=s680-w680-h510-rw',
   'https://lh3.googleusercontent.com/gps-cs-s/AG0ilSy1-uj1byJSyh7CEAflNEWN5VWghsJCzhF0hHA15Sw_UCanGtVqax-x50lfT2NTagRR6GxRDoMeixgh536Z2lhCUPR_uUDm6NmrcFdsBxnmmS5D5CvR2BMw_EPTO8zVmsVEeCLF=s680-w680-h510-rw',
   'https://lh3.googleusercontent.com/p/AF1QipPpgDSvV0COqpJ6kqtj27KdlYsmvQ8LmYpV1IWv=s680-w680-h510-rw',
+  'https://lh3.googleusercontent.com/p/AF1QipN_IVGV1pbMzzhvA6WardU3YkGRgh_eURt1GPgr=s680-w680-h510-rw',
 ];
