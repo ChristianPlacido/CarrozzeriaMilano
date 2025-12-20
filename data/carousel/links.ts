9af429e7e030afea0f082b149e922a4f694ee1f4
Christian <christian.placido@stu.fondazionejobsacademy.org>
2025-12-20 16:38:28 +0000

    Aggiunge secondo link immagine al carosello

--- data/carousel/links.ts
+++ data/carousel/links.ts
@@ -3,4 +3,5 @@
 export const CAROUSEL_LINKS: string[] = [
   // Incolla qui i tuoi link, uno per riga tra apici
   'https://lh3.googleusercontent.com/p/AF1QipONNV2jPqO1l3f6SFt9_6XXcehRPVEqBk3Z0pka=s680-w680-h510-rw',
+  'https://lh3.googleusercontent.com/p/AF1QipN4gUNCE3dsmgTc_Ur7Gqz8y3TjxdHXNO7Nd55d=s680-w680-h510-rw',
 ];
