2b1c36a719ecffddcc3bc325db5fc02a5f3356d9
Christian <christian.placido@stu.fondazionejobsacademy.org>
2025-12-20 16:42:49 +0000

    Aggiunge tutti i link immagine forniti al carosello

--- data/carousel/links.ts
+++ data/carousel/links.ts
@@ -4,4 +4,9 @@ export const CAROUSEL_LINKS: string[] = [
   // Incolla qui i tuoi link, uno per riga tra apici
   'https://lh3.googleusercontent.com/p/AF1QipONNV2jPqO1l3f6SFt9_6XXcehRPVEqBk3Z0pka=s680-w680-h510-rw',
   'https://lh3.googleusercontent.com/p/AF1QipN4gUNCE3dsmgTc_Ur7Gqz8y3TjxdHXNO7Nd55d=s680-w680-h510-rw',
+  'https://lh3.googleusercontent.com/p/AF1QipP7_kXUzFjsmsSlPmdAyyb-XsxYOaDM9ZLooKva=s680-w680-h510-rw',
+  'https://lh3.googleusercontent.com/p/AF1QipMcvzNrZ_Z2SynAV7PWf9uFNq_CqNiSbPoy4Viu=s680-w680-h510-rw',
+  'https://lh3.googleusercontent.com/gps-cs-s/AG0ilSy5_2h6JGS0Xelj1BvMBSNd_7JIoMbX3LeqfdLNOeyJ44BVtctIy_S3rbzz8xTPmGak1ToMhK6_BVtNyUS7O5y7uWGMxdZFPZnThAAjT5Gzu3azdvpphzIcfghNi3Ri9Rs4jCRP=s680-w680-h510-rw',
+  'https://lh3.googleusercontent.com/gps-cs-s/AG0ilSy1-uj1byJSyh7CEAflNEWN5VWghsJCzhF0hHA15Sw_UCanGtVqax-x50lfT2NTagRR6GxRDoMeixgh536Z2lhCUPR_uUDm6NmrcFdsBxnmmS5D5CvR2BMw_EPTO8zVmsVEeCLF=s680-w680-h510-rw',
+  'https://lh3.googleusercontent.com/p/AF1QipPpgDSvV0COqpJ6kqtj27KdlYsmvQ8LmYpV1IWv=s680-w680-h510-rw',
 ];
